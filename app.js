211ba7ccdba01aebc8176fd67fcde369d527fa93
Serge <rwibuserge@gmail.com>
2023-02-21 08:13:21 +0200

    port update

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const swaggerOptions = {
         },
         servers:[
              {
-                url:  '/'
+                url:  'https://my-brand-backend-serge.onrender.com'
             }
         ]
     
