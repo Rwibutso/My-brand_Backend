6ed53ccafd79feb82929f1e83cdd6fab2450756d
Serge <rwibuserge@gmail.com>
2023-02-21 08:01:17 +0200

    change-port

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const swaggerOptions = {
         },
         servers:[
              {
-                url:  'http://localhost:3000/'
+                url:  'http://localhost:10000/'
             }
         ]
     
